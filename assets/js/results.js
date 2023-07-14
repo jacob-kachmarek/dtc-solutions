ee6627db115fee9ac162278810dff479627b4083
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-14 14:34:04 -0700

    uncommented top of results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,19 +1,19 @@
-// var resultsContainer = document.getElementById('results-container');
+var resultsContainer = document.getElementById('results-container');
 
-// var dtcResults = localStorage.getItem("dtc");
-// var dtcParsed = JSON.parse(dtcResults);
-// var descriptionEl = document.createElement('p');
-// var potentialCauseEl = document.createElement('p');
-// var dtcCodeEl = document.createElement('p');
+var dtcResults = localStorage.getItem("dtc");
+var dtcParsed = JSON.parse(dtcResults);
+var descriptionEl = document.createElement('p');
+var potentialCauseEl = document.createElement('p');
+var dtcCodeEl = document.createElement('p');
 
-// descriptionEl.textContent = "Definition: " + dtcParsed.definition;
-// potentialCauseEl.textContent = "Potential Causes: " + dtcParsed.cause;
-// dtcCodeEl.textContent = "DTC Code: " + dtcParsed.code;
+descriptionEl.textContent = "Definition: " + dtcParsed.definition;
+potentialCauseEl.textContent = "Potential Causes: " + dtcParsed.cause;
+dtcCodeEl.textContent = "DTC Code: " + dtcParsed.code;
 
-// resultsContainer.append(dtcCodeEl, descriptionEl, potentialCauseEl);
-// console.log(dtcParsed);
+resultsContainer.append(dtcCodeEl, descriptionEl, potentialCauseEl);
+console.log(dtcParsed);
 
-// var towingContainer = document.getElementById('towing-container');
+var towingContainer = document.getElementById('towing-container');
 var map;
 var service;
 var infowindow;
