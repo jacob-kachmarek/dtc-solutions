3fd310c26f2f71f56f452e6aee7b93a971e8acfc
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-07-18 13:05:13 -0700

    updated exact location and default location

--- assets/js/results.js
+++ assets/js/results.js
@@ -39,54 +39,59 @@ var service;
 var infowindow;
 
 var markers = []; // Defined markers for the functions 
-var defaultLocation = new google.maps.LatLng(45.515232, -122.678385);
+var defaultLocation = {lat:45.515232, lng: -122.678385}; // Default location for the map
 
-function initMap() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
+function initMap() { // Calling on the locations 
+  if (navigator.geolocation) { // If the user allows the geolocation to see their location 
+    navigator.geolocation.getCurrentPosition(function (position) {
         var userLocation = {
-          lat: position.coords.latitude,
+          lat: position.coords.latitude,// Translate position  into latitude and longitude
           lng: position.coords.longitude,
         };
-        createMap(userLocation);
+        createMap(userLocation); // Creates the map for location being called 
       },
-      function (error) {
+      function (error) { // Shows error message and presents the defauled location
         console.error('Error getting user location:', error);
         createMap(defaultLocation);
       }
     );
-  } else {
+  } else { // If the user does not allow the geolocation then it will say this message and present the defauled location
     console.error('Geolocation is not supported by this browser.');
     createMap(defaultLocation);
   }
 }
 
 
-// Makes the map with the location of the user
+// Makes the map container and load the content
 function createMap(locationObj) {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: locationObj,
+    center: locationObj, //Centers the generalize location on the map
     zoom: 15,
   });
 
   var request = {
     location: locationObj,
-    radius: '500',
-    query: 'mechanics',
+    radius: '500', // How far out to search from the location 
+    query: 'mechanics', // What to search for
   };
 
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+  service = new google.maps.places.PlacesService(map);// Creates all the locations near
+  service.textSearch(request, callback); // Calls the search function
 }
 
-function callback(results, status) {
+function callback(results, status) { 
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     console.log(results);
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
       createMarker(place);
-      appendLocations(place);
+      
+      // Checkikng the appendLocations function to make sure its a function
+      // for some reason it wasnt taking it as a function before ? 
+      //appendLocations(place); <- Old call before
+      if (typeof appendLocations === 'function') {
+        appendLocations(place);
+      }
     }
   }
 }
@@ -172,5 +177,9 @@ function clearMarkers() {
   markers = [];
 }
 
-window.addEventListener('load', initMap);
-window.initMap = initMap;
+// window.addEventListener('load', initMap);
+// window.initMap = initMap;
+function loadMap() {
+  initMap();
+}
+window.addEventListener('load', loadMap);
